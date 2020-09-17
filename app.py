2fc5a93a7d1125c6672aa18ccdb7af6ccf74f866
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-09-17 17:59:12 -0600

    Add all lists query

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def delete_todo(todo_id):
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
-    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+    return render_template('index.html', lists = TodoList.query.all(), todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route('/')
 def index():
